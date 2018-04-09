e7df34f5d124c9fb850595568fd1bbe162111f4a
atikfahad <fahadatik@gmail.com>
2018-04-10 02:58:58 +0600

    Boundary is working for straight line..

--- app/src/main/java/com/dxball/dxballpractise/GameCanvas.java
+++ app/src/main/java/com/dxball/dxballpractise/GameCanvas.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 
 
-public class GameCanvas extends View {
+public class GameCanvas extends View implements KeyEvent.Callback {
     Paint paint;
     float maxWidth, maxHeight;
     float x, y, dx = 1, dy = 1;
@@ -15,6 +17,7 @@ public class GameCanvas extends View {
     float radius = 20;
     boolean flag = true;
     boolean up = false, down = true, left = false, right = false;
+    boolean keyLeft = false, keyRight = false;
 
     protected void onDraw(Canvas canvas){
         if (ifFirst){
@@ -28,25 +31,43 @@ public class GameCanvas extends View {
         canvas.drawRGB(255,255,255);
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.FILL);
+        if(keyRight){
+            keyRight = false;
+            right = true;
+            left = up = down = false;
+        }
+        if(keyLeft){
+            keyLeft = false;
+            left = true;
+            right = up = down = false;
+        }
         if(y == maxHeight - radius){
             down = false;
             up = true;
         }
         if(down && y > 0) {
-            canvas.drawCircle(x, y++, radius, paint);
+            canvas.drawCircle(x, y+=3, radius, paint);
         }
         else if(up && y <= maxHeight - radius) {
-            canvas.drawCircle(x, y--, radius, paint);
+            canvas.drawCircle(x, y-=3, radius, paint);
             if(y == radius){
                 up = false;
                 down = true;
             }
         }
-        else if(left && x > 0){
-            canvas.drawCircle(x--, y,radius, paint);
+        else if(left){
+            canvas.drawCircle(x-=3, y,radius, paint);
+            if(x <= radius){
+                left = false;
+                right = true;
+            }
         }
-        else if(right && x <= maxWidth - radius){
-            canvas.drawCircle(x++, y, radius, paint);
+        else if(right){
+            canvas.drawCircle(x+=3, y, radius, paint);
+            if(x >= maxWidth - radius){
+                right = false;
+                left = true;
+            }
         }
         invalidate();
 
@@ -55,5 +76,33 @@ public class GameCanvas extends View {
     public GameCanvas(Context context) {
         super(context);
         paint = new Paint();
+    }
+
+//    @Override
+//    public boolean onKey(View view, int keyCode, KeyEvent event) {
+//        switch(event.getAction())
+//        {
+//            case KeyEvent.KEYCODE_VOLUME_DOWN:
+//                this.keyLeft = true;
+//                Log.d("1.", "onKey: volume_pressed_down");
+//                break;
+//            case KeyEvent.KEYCODE_VOLUME_UP:
+//                Log.d("2.", "onKey: volume_pressed_up");
+//                this.keyRight = true;
+//                break;
+//        }
+//        return false;
+//    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)){
+            this.keyLeft = true;
+            Log.d("1.", "onKey: volume_pressed_down");
+        }
+        else if ((keyCode == KeyEvent.KEYCODE_VOLUME_UP)){
+            this.keyRight = true;
+            Log.d("1.", "onKey: volume_pressed_up");
+        }
+        return true;
     }
 }
--- app/src/main/java/com/dxball/dxballpractise/MainActivity.java
+++ app/src/main/java/com/dxball/dxballpractise/MainActivity.java
@@ -2,15 +2,20 @@ package com.dxball.dxballpractise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.view.KeyEvent;
 import android.view.WindowManager;
+import android.view.View.OnKeyListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnKeyListener {
+    GameCanvas g = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GameCanvas g = new GameCanvas(this);
+        g = new GameCanvas(this);
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -18,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
         g.requestFocus();
         setContentView(g);
         //setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onKey(View view, int keyCode, KeyEvent event) {
+        switch(event.getAction())
+        {
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                g.keyLeft = true;
+                Log.d("1.", "onKey: volume_pressed_down");
+                break;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                Log.d("2.", "onKey: volume_pressed_up");
+                g.keyRight = true;
+                break;
+        }
+        return false;
     }
 }
